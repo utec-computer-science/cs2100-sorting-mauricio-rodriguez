dd15431f4990e8598fb7f632298cd0cea08fc472
Mauricio Rodriguez <mauricio.rodriguez@utec.edu.pe>
2020-03-30 16:47:29 -0500

    feat(insertionSort): insertion sort completed for basic class and vector of integers

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,1 +1,14 @@
 #include "Sorting.h"
+void Sorting::insertionSort() {
+    int temp;
+    int j;
+    for (int i =prim+1;i<ult;i++){
+        temp = container[i];
+        j = i-1;
+        while (j>=prim && temp<container[j]) {
+            container[j + 1] = container[j];
+            j-=1;
+        }
+        container[j+1] = temp;
+    }
+}
--- Sorting.h
+++ Sorting.h
@@ -3,11 +3,16 @@
 #define CS2100_SORTING_MAURICIO_RODRIGUEZ_SORTING_H
 
 #include <vector>
+#include <iostream>
 class Sorting {
 private:
-    std::vector<int> container;
+    std::vector<int> &container;
+    int prim;
+    int ult;
 public:
-    Sorting(std::vector<int> &container) : container(container) {}
+    Sorting(std::vector<int> &container, const int &prim,const int &ult) : container(container),
+    prim(prim),ult(ult){insertionSort();};
+    void insertionSort();
 };
 
 
